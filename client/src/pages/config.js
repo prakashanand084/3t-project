bfaaf552303406a36479aa3b3948f26c2911a5a9
PRAKASH ANAND <134529932+prakashanand084@users.noreply.github.com>
2025-05-04 00:21:46 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,4 +1,4 @@
-  const API_BASE_URL = "http://54.174.230.18:84";
+const API_BASE_URL = "http://54.174.230.18:84";
 // const API_BASE_URL = "http://veera.narni.co.in";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
