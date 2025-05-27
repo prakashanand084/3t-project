3efb28646d267ffa2a366d5d1dbf029531eafde3
PRAKASH ANAND <134529932+prakashanand084@users.noreply.github.com>
2025-05-27 11:25:57 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,8 +1,8 @@
 //const API_BASE_URL = "http://52.90.3.255:84";
-// const API_BASE_URL = "http://veera.narni.co.in";
+ const API_BASE_URL = "http://anand.975050210764.realhandsonlabs.net";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 // export default API_BASE_URL;
-const API_BASE_URL = "REACT_APP_API_BASE_URL_PLACEHOLDER";
+//const API_BASE_URL = "REACT_APP_API_BASE_URL_PLACEHOLDER";
 export default API_BASE_URL;
 
