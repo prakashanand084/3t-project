653c2d6a441bd0c6b93ad6353fd5bfc948efdfce
PRAKASH ANAND <134529932+prakashanand084@users.noreply.github.com>
2025-06-25 23:29:55 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
 //const API_BASE_URL = "http://52.90.3.255:84";
- const API_BASE_URL = "http://anand.654654454176.realhandsonlabs.net";
+ const API_BASE_URL = "http://anand.767397822919.realhandsonlabs.net";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 // export default API_BASE_URL;
